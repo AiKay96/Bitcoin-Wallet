2d3c869c5fd6d28ea8b7cf871a74e543bbe3fca5
lmebo17 <lmebo21@freeuni.edu.ge>
2024-02-01 13:14:11 +0400

    Added transaction repository.

---
+++ core/transaction.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field
+from typing import Protocol
+from uuid import UUID, uuid4
+
+
+@dataclass
+class Transaction:
+    wallet_from: UUID
+    wallet_to: UUID
+    amount: float
+
+
+class TransactionRepository(Protocol):
+    pass
+
+
+@dataclass
+class TransactionService:
+    transactions: TransactionRepository
+
+
+
+
+
