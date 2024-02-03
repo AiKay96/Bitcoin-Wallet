54b6d2e99212b03c20ad9c0038a49f6099d4d174
lmebo17 <lmebo21@freeuni.edu.ge>
2024-02-03 13:31:39 +0400

    added user database, not tested yet

---
+++ infra/in_database/user_sqlite.py
@@ -0,0 +1,56 @@
+import sqlite3
+from dataclasses import dataclass
+from uuid import UUID
+
+from core.errors import DoesNotExistError, ExistsError
+from core.users import User
+
+
+@dataclass
+class UserInDatabase:
+    db_path: str = "./database.db"
+
+    def create_table(self) -> None:
+        create_table_query = """
+            CREATE TABLE IF NOT EXISTS users (
+                id TEXT PRIMARY KEY,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                API_key TEXT NOT NULL,
+                wallets_number INTEGER NOT NULL
+            );
+        """
+        with sqlite3.connect(self.db_path) as connection:
+            cursor = connection.cursor()
+            cursor.execute(create_table_query)
+
+    def clear_tables(self) -> None:
+        truncate_units_query = """
+            DELETE FROM users;
+        """
+        with sqlite3.connect(self.db_path) as connection:
+            cursor = connection.cursor()
+            cursor.execute(truncate_units_query)
+            connection.commit()
+
+    def create(self, user: User) -> User:
+        create_user_query = '''
+            INSERT INTO users (API_KEY, username, password, wallets_number)
+            VALUES (?, ?, ?, ?, ?)
+        '''
+
+        with sqlite3.connect(self.db_path) as connection:
+            cursor = connection.cursor()
+
+            cursor.execute('SELECT * FROM users WHERE username = ?', (user.username,))
+            existing_user = cursor.fetchone()
+            if existing_user:
+                raise ExistsError("User already exists.")
+
+            cursor.execute(create_user_query, (str(user.API_key), user.username, user.password, str(user.API_key), user.wallets_number))
+            connection.commit()
+
+        return user
+
+
+
